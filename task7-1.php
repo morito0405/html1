f9f2c5fead9ad9517f2c5ddb111630d4eb3a1a2f
morito0405 <134045017+morito0405@users.noreply.github.com>
2023-07-10 12:11:08 +0900

    task7-2がほぼ完了いたしました。
    時給が横に表示されないのでまた確認して再提出します。

--- task7-1.php
+++ task7-1.php
@@ -8,6 +8,7 @@
 <body>
 <?php 
 ini_set('display_errors', 1);
+//クラスは関数のアップグレード版。定義
 class Staff{
   private $name;
   private $age;
@@ -19,20 +20,29 @@ public function  __construct($name,$age,$sex){
   $this->name = $name;
   $this->age  = $age;
   $this->sex  = $sex;
-  self ::$count++;
-  $this->id = str_pad(self ::$count, 4, "0",STR_PAD_LEFT);
+  // self ::$count++;
+  // $this->id = str_pad(self ::$count, 4, "0",STR_PAD_LEFT);
+  /*29行目に printf("(S%04d)と記載することで23行目は記載しなくて良くなる*/
+  $this->id = self ::$count++;
 }
+//関数はClassで定義したものを使う。
 function show(){
-  printf("(S%s) %s %d歳 %s<br>\n", $this->id,$this->name,$this->age,$this->sex ==='男' ? '男性':'女性' );
+  printf("(S%04d) %s %d歳 %s<br>\n", $this->id,$this->name,$this->age,$this->sex ==='男' ? '男性':'女性' );
 }
 }
-$staff1= new Staff("佐藤 一郎",31,'男');
-$staff2= new Staff("山田 花子",25,'女');
-$staff3= new Staff("鈴木 次郎",27,'男');
+$staff = [];
+$staff[1]= new Staff("佐藤 一郎",31,'男');
+$staff[2]= new Staff("山田 花子",25,'女');
+$staff[3]= new Staff("鈴木 次郎",27,'男');
+$staff[4]= new Staff("鈴木 次郎",27,'男');
+$staff[5]= new Staff("鈴木 次郎",27,'男');
+foreach($staff as $member){
+  $member->show();
+}
 
-$staff1->show();
-$staff2->show();
-$staff3->show();
+// $staff[1]->show();
+// $staff[2]->show();
+// $staff[3]->show();
 ?>
 </body>
 </html>
---
+++ task7-2.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+<?php
+ini_set('display_errors', 1);
+
+class Staff{
+  private $name;
+  private $age;
+  private $sex;
+  private static $count = 0;
+  private $id;
+
+  public function __construct($name,$age,$sex){
+    $this->name = $name;
+    $this->age = $age;
+    $this->sex = $sex;
+    self ::$count++;
+    if ($this instanceof PartStaff) {
+      $this->id = sprintf("P%04d", self::$count);
+    } else {
+      $this->id = sprintf("S%04d", self::$count);
+    }
+
+  }
+
+ public function show(){
+    printf("(S%s) %s %d歳 %s<br>\n",$this->id, $this->name,$this->age,$this->sex === '男' ? '男性' : '女性');
+  }
+}
+class PartStaff extends Staff{
+  private $jikyu;
+
+  public function  __construct($name,$age,$sex,$jikyu){
+    parent:: __construct($name,$age,$sex);
+    $this->jikyu = $jikyu;
+  }
+  
+  public function show (){
+    parent ::show();
+    printf("時給:%d円<br>",$this->jikyu);
+  }
+}
+
+$staff1 = new Staff("佐藤 一郎",31,'男');
+$staff2 = new Staff("山田 花子",25,'女');
+$staff3 = new Staff("鈴木 次郎",27,'男');
+
+$partStaff = new PartStaff("田中 友子", 24, '女',900);
+$staff4 = new Staff("中村 三郎", 25,'男');
+
+$staff1->show();
+$staff2->show();
+$staff3->show();
+$partStaff->show();
+$staff4->show();
+
+  ?>
+
+</body>
+</html>
